19d03661b04a227efb286a0fb1368dd6c8c5daba
Ian <iangonsalves8@gmail.com>
2022-06-03 19:24:10 -0300

    Refactored and completed collectPoliticianTrades method

--- SenHandlers.py
+++ SenHandlers.py
@@ -1,5 +1,4 @@
 import json
-from pydoc import pager
 import requests
 
 MIN_TRADES_WANTED = 20
@@ -7,19 +6,28 @@ ALL_POLITICIANS_API = "https://bff.capitoltrades.com/politicians?pageSize=all&pa
 TRADE_API_START = "https://bff.capitoltrades.com/trades?page="
 TRADE_API_END = "&pageSize=100&politician="
 PAGE_START_NUM = "1"
-
-# Politician trades api = https://bff.capitoltrades.com/trades?politician=C001075&txDate=all
+API_KEYWORDS = [
+        "data",
+        "stats",
+        "meta",
+        "countTrades",
+        "firstName",
+        "lastName",
+        "_politicianId",
+        "paging",
+        "totalPages"
+]
 
 def getPoliticians():
     pols = []
     rawData = requests.get(ALL_POLITICIANS_API).text
-    data = json.loads(rawData)["data"]
+    data = json.loads(rawData)[API_KEYWORDS[0]]
 
     for polData in data:
-        countTrades = polData["stats"]["countTrades"]
+        countTrades = polData[API_KEYWORDS[1]][API_KEYWORDS[3]]
         if(countTrades > MIN_TRADES_WANTED):
-            fullName = polData["firstName"] + " " + polData["lastName"]
-            pols.append({"_politicianId" : polData["_politicianId"], "fullName" : fullName})    
+            fullName = polData[API_KEYWORDS[4]] + " " + polData[API_KEYWORDS[5]]
+            pols.append({API_KEYWORDS[6] : polData[API_KEYWORDS[6]], "fullName" : fullName})    
 
     return pols
 
@@ -29,9 +37,9 @@ def getPoliticianTrades(polID):
     startNum = 1
     tradesURL = tradeAPI(startNum, polID)
     req = requests.get(tradesURL)
-    trades = json.loads(req.text)["data"]
-    meta = json.loads(req.text)["meta"]
-    pages = meta["paging"]["totalPages"]
+    trades = json.loads(req.text)[API_KEYWORDS[0]]
+    meta = json.loads(req.text)[API_KEYWORDS[2]]
+    pages = meta[API_KEYWORDS[7]][API_KEYWORDS[8]]
 
     if (pages > startNum):
         allTrades = []
@@ -41,12 +49,11 @@ def getPoliticianTrades(polID):
         while startNum <= pages:
             tradesURL = tradeAPI(startNum, polID)
             req = requests.get(tradesURL)
-            trades = json.loads(req.text)["data"]
+            trades = json.loads(req.text)[API_KEYWORDS[0]]
             allTrades.append(trades)
             startNum += 1
         
         return allTrades
-
     
     else:
         return trades
@@ -61,12 +68,14 @@ def collectPoliticianTrades():
     politicianTrades = []
     politicians = getPoliticians()
 
-    print(getPoliticianTrades(politicians[0]["_politicianId"]))
+    for pol in politicians:
+        trades = getPoliticianTrades(pol[API_KEYWORDS[6]])
+        politicianTrades.append({"politician" : pol, "trades" : trades})
+
+    return politicianTrades
 
 
 
-
-collectPoliticianTrades()
 
 
 #Grading system for politicians that calculates performance of individual 
